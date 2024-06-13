c2f8a8cd63dea207398c0b157d63debfd813d50f
Carlos Adriano Portes <xcap2000@outlook.com>
2024-06-13 00:06:00 -0300

    Add simple functionality

--- source/app.d
+++ source/app.d
@@ -2,13 +2,41 @@ import gtk.MainWindow;
 import gtk.Label;
 import gtk.Main;
 import std.stdio;
+import gtk.Button;
+import gtk.Box;
+import gtk.Entry;
+
+// import core.interpolation;
+import std.conv : text;
 
 void main(string[] args)
 {
     Main.init(args);
     MainWindow win = new MainWindow("Hello World");
+    auto box = new Box(GtkOrientation.VERTICAL, 0);
     win.setDefaultSize(200, 100);
-    win.add(new Label("Hello World"));
+    auto label = new Label("Hello");
+    box.add(label);
+    auto entry = new Entry();
+    box.add(entry);
+    auto button = new Button("Hello!");
+    // button.addOnPressed((Button b) => label.setText("Hello " ~ entry.getText ~ "!!!"));
+    button.addOnPressed((Button b) => label.setText(i"Hello $(entry.getText())!!!".text));
+    // button.addOnPressed(delegate(Button b) {
+    //     auto message = i"Hello $(entry.getText())!!!!";
+    //     auto message_text = message.text;
+    //     // string message_text = "Hello " ~ entry.getText ~ "!!!";
+    //     label.setText(message_text);
+    // });
+    box.add(button);
+    win.add(box);
     win.showAll();
     Main.run();
 }
+
+// void button_pressed(Button b)
+// {
+//     auto message = i"Hello $(entry.getText)!!!";
+//     auto message_text = message.text;
+//     label.setText(text);
+// }
